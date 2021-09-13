e7aa590998811958e575b395ee6ed588dd9de22d
majianjie <majianjie@didiglobal.com>
2021-09-13 09:10:03 +0800

    Feat: init

---
+++ DDUtil.h
@@ -0,0 +1,16 @@
+//
+//  Util.h
+//  DDNetWork
+//
+//  Created by majianjie on 2021/9/13.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DDUtil : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DDUtil.m
@@ -0,0 +1,12 @@
+//
+//  Util.m
+//  DDNetWork
+//
+//  Created by majianjie on 2021/9/13.
+//
+
+#import "DDUtil.h"
+
+@implementation DDUtil
+
+@end
